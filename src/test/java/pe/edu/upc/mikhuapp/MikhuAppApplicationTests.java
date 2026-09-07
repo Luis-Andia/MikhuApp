7c8163e77c917fa3acc3d48743fa45e27d637379
Luis_AS <u202417048@upc.edu.pe>
2026-09-07 11:28:49 -0500

    Initial commit

---
+++ src/test/java/pe/edu/upc/mikhuapp/MikhuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.mikhuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikhuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
