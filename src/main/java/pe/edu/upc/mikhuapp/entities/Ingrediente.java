2f231463660a30cad4b050f3c0d6635450f5488a
bianzelnoriegad <bianzelnoriegadonayre@gmail.com>
2026-09-13 23:28:15 -0500

    Ingrediente.java

---
+++ src/main/java/pe/edu/upc/mikhuapp/entities/Ingrediente.java
@@ -0,0 +1,41 @@
+package pe.edu.upc.mikhuapp.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Ingrediente")
+public class Ingrediente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idIngrediente;
+
+    @Column(name = "nomIngrediente", length = 40, nullable = false)
+    private String nomIngrediente;
+
+    // Constructores
+
+    public Ingrediente() {
+    }
+
+    public Ingrediente(Long idIngrediente, String nomIngrediente) {
+        this.idIngrediente = idIngrediente;
+        this.nomIngrediente = nomIngrediente;
+    }
+
+    public Long getIdIngrediente() {
+        return idIngrediente;
+    }
+
+    public void setIdIngrediente(Long idIngrediente) {
+        this.idIngrediente = idIngrediente;
+    }
+
+    public String getNomIngrediente() {
+        return nomIngrediente;
+    }
+
+    public void setNomIngrediente(String nomIngrediente) {
+        this.nomIngrediente = nomIngrediente;
+    }
+}
