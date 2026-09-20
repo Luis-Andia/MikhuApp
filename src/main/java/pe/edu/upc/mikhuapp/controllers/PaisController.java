5578234728e9e9605c03244d43437d8a443a6de9
Luis_AS <u202417048@upc.edu.pe>
2026-09-20 18:14:58 -0500

    Cambiar idioma a las variables 3

--- src/main/java/pe/edu/upc/mikhuapp/controllers/PaisController.java
+++ src/main/java/pe/edu/upc/mikhuapp/controllers/PaisController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import pe.edu.upc.mikhuapp.dtos.PaisDTO;
+import pe.edu.upc.mikhuapp.dtos.CountryDTO;
 import pe.edu.upc.mikhuapp.entities.Country;
 import pe.edu.upc.mikhuapp.exceptions.ResourceNotFoundException;
 import pe.edu.upc.mikhuapp.servicesinterfaces.IPaisService;
@@ -29,11 +29,11 @@ public class PaisController {
 
     // REGISTRAR NUEVO PAIS
     @PostMapping
-    public ResponseEntity<PaisDTO> registrar(@Validated @RequestBody PaisDTO dto){
+    public ResponseEntity<CountryDTO> registrar(@Validated @RequestBody CountryDTO dto){
         Country nuevo_country = modelMapper.map(dto, Country.class);
         pS.insert(nuevo_country);
 
-        PaisDTO responseDTO = modelMapper.map(nuevo_country, PaisDTO.class);
+        CountryDTO responseDTO = modelMapper.map(nuevo_country, CountryDTO.class);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("{/id}")
@@ -46,32 +46,32 @@ public class PaisController {
 
     // LISTAR PAISES
     @GetMapping
-    public ResponseEntity<List<PaisDTO>> listar_paises(){
-        List<PaisDTO> lista_paises = pS.list()
+    public ResponseEntity<List<CountryDTO>> listar_paises(){
+        List<CountryDTO> lista_paises = pS.list()
                 .stream()
-                .map(p -> modelMapper.map(p, PaisDTO.class))
+                .map(p -> modelMapper.map(p, CountryDTO.class))
                 .toList();
         return ResponseEntity.ok(lista_paises);
     }
 
     // ACTUALIZAR PAIS
     @PutMapping("/{id}")
-    public ResponseEntity<PaisDTO> actualizar_pais(@PathVariable("id") long id, @Validated @RequestBody PaisDTO dto){
+    public ResponseEntity<CountryDTO> actualizar_pais(@PathVariable("id") long id, @Validated @RequestBody CountryDTO dto){
 
         // AGREGAR VALIDACION DE ID valido
         Country country = modelMapper.map(dto, Country.class);
         country.setIdCountry(id);
         pS.update(country);
-        PaisDTO responseDTO = modelMapper.map(country, PaisDTO.class);
+        CountryDTO responseDTO = modelMapper.map(country, CountryDTO.class);
         return ResponseEntity.ok(responseDTO);
     }
 
     // CONSULTAR PAIS POR ID
     @GetMapping("/{id}")
-    public ResponseEntity<PaisDTO> buscar_pais_id(@PathVariable Long id){
+    public ResponseEntity<CountryDTO> buscar_pais_id(@PathVariable Long id){
         Country p = pS.listid(id)
                 .orElseThrow(()->new ResourceNotFoundException("No existe el pais"));
-        PaisDTO responseDTO = modelMapper.map(p, PaisDTO.class);
+        CountryDTO responseDTO = modelMapper.map(p, CountryDTO.class);
         return ResponseEntity.ok(responseDTO);
     }
 
---
+++ src/main/java/pe/edu/upc/mikhuapp/dtos/CountryDTO.java
@@ -0,0 +1,35 @@
+package pe.edu.upc.mikhuapp.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class CountryDTO {
+    private Long idCountry;
+    @NotBlank(message = "El codigo del pais no puede estar vacio")
+    private String codCountry;
+    @NotBlank(message = "El nombre del pais no puede estar vacio")
+    private String nomCountry;
+
+    public Long getIdCountry() {
+        return idCountry;
+    }
+
+    public void setIdCountry(Long idCountry) {
+        this.idCountry = idCountry;
+    }
+
+    public String getCodCountry() {
+        return codCountry;
+    }
+
+    public void setCodCountry(String codCountry) {
+        this.codCountry = codCountry;
+    }
+
+    public String getNomCountry() {
+        return nomCountry;
+    }
+
+    public void setNomCountry(String nomCountry) {
+        this.nomCountry = nomCountry;
+    }
+}
