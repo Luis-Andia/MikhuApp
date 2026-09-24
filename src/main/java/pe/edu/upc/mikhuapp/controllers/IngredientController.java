ee637aadedd7d103b806451000fd218d874ace57
Luis_AS <u202417048@upc.edu.pe>
2026-09-24 00:10:57 -0500

    Mejoras a la entidad IngredientController

--- src/main/java/pe/edu/upc/mikhuapp/controllers/IngredientController.java
+++ src/main/java/pe/edu/upc/mikhuapp/controllers/IngredientController.java
@@ -3,6 +3,7 @@ package pe.edu.upc.mikhuapp.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -17,28 +18,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/ingredientes")
 public class IngredientController {
+    private final IIngredientService iS;
+    private final ModelMapper modelMapper;
 
-    @Autowired
-    private IIngredientService ingredienteService;
-
-    @Autowired
-    private ModelMapper modelMapper;
-
-    @GetMapping
-    public ResponseEntity<List<IngredientDTO>> list() {
-        List<IngredientDTO> lista = ingredienteService.list().stream()
-                .map(ingrediente -> modelMapper.map(ingrediente, IngredientDTO.class))
-                .toList();
-
-        return ResponseEntity.ok(lista);
+    public IngredientController(IIngredientService iS, ModelMapper modelMapper) {
+        this.iS = iS;
+        this.modelMapper = modelMapper;
     }
 
+    // HU21: Registrar Ingrediente
     @PostMapping
     public ResponseEntity<IngredientDTO> insert(@Validated @RequestBody IngredientDTO dto) {
 
         Ingredient ingredient = modelMapper.map(dto, Ingredient.class);
 
-        Ingredient ingredientRegistrado = ingredienteService.insert(ingredient);
+        Ingredient ingredientRegistrado = iS.insert(ingredient);
 
         IngredientDTO response = modelMapper.map(ingredientRegistrado, IngredientDTO.class);
 
@@ -51,14 +45,28 @@ public class IngredientController {
         return ResponseEntity.created(location).body(response);
     }
 
+    // HU22: Listar ingredientes registrados
+    @GetMapping
+    public ResponseEntity<List<IngredientDTO>> list() {
+        List<IngredientDTO> lista = iS.list().stream()
+                .map(ingredient -> modelMapper.map(ingredient, IngredientDTO.class))
+                .toList();
+
+        return ResponseEntity.ok(lista);
+    }
+
+    // HU23: Actualizar un ingrediente
+
+    // HU24: Eliminar un ingrediente
+
+    // HU25: Consultar un ingredinete por ID
     @GetMapping("/{id}")
     public ResponseEntity<IngredientDTO> listId(@PathVariable Long id) {
 
-        Ingredient ingredient = ingredienteService.listid(id)
+        Ingredient ingredient = iS.listid(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Ingrediente no encontrado"));
 
         IngredientDTO dto = modelMapper.map(ingredient, IngredientDTO.class);
-
         return ResponseEntity.ok(dto);
     }
 }
