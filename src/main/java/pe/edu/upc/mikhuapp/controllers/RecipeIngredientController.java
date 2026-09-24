814f126ec90c292306ecd4985dfe365c08badc17
Luis_AS <u202417048@upc.edu.pe>
2026-09-24 02:06:15 -0500

    Parche de Entidad RecipeIngredientServiceImplements

--- src/main/java/pe/edu/upc/mikhuapp/controllers/RecipeIngredientController.java
+++ src/main/java/pe/edu/upc/mikhuapp/controllers/RecipeIngredientController.java
@@ -1,11 +1,9 @@
 package pe.edu.upc.mikhuapp.controllers;
 
 import jakarta.validation.Valid;
-import org.hibernate.validator.constraints.ParameterScriptAssert;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pe.edu.upc.mikhuapp.dtos.RecipeIngredientDTO;
 import pe.edu.upc.mikhuapp.entities.Ingredient;
 import pe.edu.upc.mikhuapp.entities.Recipe;
@@ -15,7 +13,6 @@ import pe.edu.upc.mikhuapp.servicesinterfaces.IIngredientService;
 import pe.edu.upc.mikhuapp.servicesinterfaces.IRecipeIngredientService;
 import pe.edu.upc.mikhuapp.servicesinterfaces.IRecipeService;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
