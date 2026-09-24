70d776c835c545dc24d8af0592eb18729cf171b3
JohanJ <u202418680@upc.edu.pe>
2026-09-24 12:57:23 -0500

    Avance de HU04, HU38, HU39

--- src/main/java/pe/edu/upc/mikhuapp/servicesimplements/ConsumptionServiceImplements.java
+++ src/main/java/pe/edu/upc/mikhuapp/servicesimplements/ConsumptionServiceImplements.java
@@ -3,6 +3,7 @@ package pe.edu.upc.mikhuapp.servicesimplements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pe.edu.upc.mikhuapp.entities.Consumption;
+import pe.edu.upc.mikhuapp.entities.Recipe;
 import pe.edu.upc.mikhuapp.repositories.IConsumptionRepository;
 import pe.edu.upc.mikhuapp.servicesinterfaces.IConsumptionService;
 
@@ -36,5 +37,15 @@ public class ConsumptionServiceImplements implements IConsumptionService {
     @Override
     public List<Object[]> ListMostConsumedIngredients() {
         return cR.ListMostConsumedIngredients();
+    }
+
+    @Override
+    public void update(Consumption consumption) {
+        cR.save(consumption);
+    }
+
+    @Override
+    public void delete(Long id) {
+        cR.deleteById(id);
     }
 }
