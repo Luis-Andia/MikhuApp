c796226a3042602a3a665567c1e24652afdd4300
TeroZ14 <jheisonmonterozarate4@gmail.com>
2026-09-24 02:16:49 -0500

    Implementacion HU11

--- src/main/java/pe/edu/upc/mikhuapp/entities/Recipe.java
+++ src/main/java/pe/edu/upc/mikhuapp/entities/Recipe.java
@@ -10,31 +10,26 @@ public class Recipe {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idRecipe;
 
-    @Column(name="nomRecipe", length = 100, nullable = false)
+    @Column(name="Nom_Receta", length = 100, nullable = false)
     private String nomRecipe;
 
     @ManyToOne
-    @JoinColumn(name="idCountry")
+    @JoinColumn(name="Id_Pais")
     private Country country;
 
-    @Column(name="calories", nullable = false)
-    private double calories;
-
-    @Column(name="difficulty", nullable = false)
-    private int difficulty;
+    @Column(name="Calorias", nullable = false)
+    private Integer calories;
 
     public Recipe() {
     }
 
-    public Recipe(Long idRecipe, String nomRecipe, Country country, double calories, int difficulty) {
+    public Recipe(Long idRecipe, String nomRecipe, Country country,
+                  Integer calories) {
         this.idRecipe = idRecipe;
         this.nomRecipe = nomRecipe;
         this.country = country;
         this.calories = calories;
-        this.difficulty = difficulty;
     }
-
-    // get and set
 
     public Long getIdRecipe() {
         return idRecipe;
@@ -52,27 +47,20 @@ public class Recipe {
         this.nomRecipe = nomRecipe;
     }
 
-    public Country getCountry() {
+    public Country getPais() {
         return country;
     }
 
-    public void setCountry(Country country) {
+    public void setPais(Country country) {
         this.country = country;
     }
 
-    public double getCalories() {
+    public Integer getCalories() {
         return calories;
     }
 
-    public void setCalories(double calories) {
+    public void setCalories(Integer calories) {
         this.calories = calories;
     }
 
-    public int getDifficulty() {
-        return difficulty;
-    }
-
-    public void setDifficulty(int difficulty) {
-        this.difficulty = difficulty;
-    }
 }
