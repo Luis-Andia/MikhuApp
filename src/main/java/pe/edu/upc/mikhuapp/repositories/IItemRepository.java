63e3365fb7b595fdcc6fc73a413a917c2650e9fc
bianzelnoriegad <bianzelnoriegadonayre@gmail.com>
2026-09-24 13:29:31 -0500

    Corrección HU47

--- src/main/java/pe/edu/upc/mikhuapp/repositories/IItemRepository.java
+++ src/main/java/pe/edu/upc/mikhuapp/repositories/IItemRepository.java
@@ -6,12 +6,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.mikhuapp.entities.Item;
 
-import java.security.PublicKey;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface IItemRepository extends JpaRepository<Item, Long> {
@@ -30,7 +26,7 @@ public interface IItemRepository extends JpaRepository<Item, Long> {
 
     //HU47 Listar items del inventario familiar
     @Query(value = "SELECT i FROM Item i"
-            + " WHERE i.family.idFamily = :idFamily"
+            + " WHERE i.familia.idFamily = :idFamily"
     )
     List<Item> findByFamilyId(
             @Param("idFamily") Long idFamily
